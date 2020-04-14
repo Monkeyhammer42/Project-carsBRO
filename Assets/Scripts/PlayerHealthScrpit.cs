d24913dd1f46381d99407ebda2594d5ce598b433
monkeyhammer42 <michael-whiddett@hotmail.com>
2020-04-14 12:29:51 +0100

    HealthScript Ui Changes

---
+++ Assets/Scripts/PlayerHealthScrpit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthScrpit : MonoBehaviour
+
+{
+    public int healthValue = 100;
+    public Slider health_Slider;
+    public Text _healthText;
+
+    public GameObject UI_Holder;
+
+    void Start()
+    {
+
+
+        health_Slider.value = healthValue;
+
+
+    }
+    private void Update()
+    {
+        _healthText.text = "Health : " + healthValue;
+    }
+
+    public void ApplyDamage(int damageAmount)
+    {
+        healthValue -= damageAmount;
+        
+        if (healthValue < 0)
+        {
+            healthValue = 0;
+        }
+        health_Slider.value = healthValue;
+        if (healthValue == 0)
+        {
+            UI_Holder.SetActive(true);
+
+        }
+
+    }
+}
